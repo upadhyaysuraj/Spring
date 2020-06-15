04aaa2d3741c768933b5faebf87b38249456d1ee
Suraj <su.suraj.upadhyay@gmail.com>
2020-06-15 19:47:23 -0400

    MultiAction controller class (Can not have same URL String mapped to different methods)
    Class level Request URL mapping (URL pre-fix like)

--- Spring_05_FirstWebApp/src/org/suraj/controller/WelcomeController.java
+++ Spring_05_FirstWebApp/src/org/suraj/controller/WelcomeController.java
@@ -5,15 +5,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/H")
 public class WelcomeController
 {
+	
+	@RequestMapping("/m")
+	public ModelAndView m1()
+	{
+		ModelAndView mNv = new ModelAndView("welcome");
+		mNv.addObject("username", "Upadhyay");
+		System.out.println("In m1()");
+		return mNv;
+	}
 
 	@RequestMapping("/")
 	public ModelAndView m()
 	{
 		ModelAndView mNv = new ModelAndView("welcome");
 		mNv.addObject("username", "Suraj");
+		System.out.println("In m()");
 		return mNv;
 	}
-	
 }
