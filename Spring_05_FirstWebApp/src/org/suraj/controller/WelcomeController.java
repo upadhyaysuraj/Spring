e5a313f1033d52e34bacc7a0d7a1e247f9bf99ad
Suraj <su.suraj.upadhyay@gmail.com>
2020-06-15 19:09:55 -0400

    Spring MVC -> Annotation based configuration of a Controller's method mapped to a URL request

--- Spring_05_FirstWebApp/src/org/suraj/controller/WelcomeController.java
+++ Spring_05_FirstWebApp/src/org/suraj/controller/WelcomeController.java
@@ -1,16 +1,15 @@
 package org.suraj.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-public class WelcomeController extends AbstractController
+@Controller
+public class WelcomeController
 {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
-			throws Exception
+	@RequestMapping("/")
+	public ModelAndView m()
 	{
 		ModelAndView mNv = new ModelAndView("welcome");
 		mNv.addObject("username", "Suraj");
