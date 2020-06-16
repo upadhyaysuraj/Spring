dbdae0f37f75fa77e4fb5fd06fdbb85dcaafd2df
Suraj <su.suraj.upadhyay@gmail.com>
2020-06-16 10:25:21 -0400

    Path value parameters! -> using from Map<String, String> {key = path value name}

--- Spring_05_FirstWebApp/Web/WEB-INF/jsp/welcome.jsp
+++ Spring_05_FirstWebApp/Web/WEB-INF/jsp/welcome.jsp
@@ -7,6 +7,10 @@
 <title>Welcome</title>
 </head>
 <body>
-Welcome ${username}
+Welcome ${username}.
+
+You have a path parameter -> ${pathValue1}
+You have another path parameter -> ${pathValue2}
+
 </body>
 </html>
--- Spring_05_FirstWebApp/src/org/suraj/controller/WelcomeController.java
+++ Spring_05_FirstWebApp/src/org/suraj/controller/WelcomeController.java
@@ -1,11 +1,13 @@
 package org.suraj.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/H")
 public class WelcomeController
 {
 	
@@ -24,6 +26,16 @@ public class WelcomeController
 		ModelAndView mNv = new ModelAndView("welcome");
 		mNv.addObject("username", "Suraj");
 		System.out.println("In m()");
+		return mNv;
+	}
+	
+	@RequestMapping("/{pathValue1}/{pathValue2}")
+	public ModelAndView path(@PathVariable Map<String, String> vars)
+	{
+		ModelAndView mNv = new ModelAndView("welcome");
+		mNv.addObject("username", "Suraj");
+		mNv.addObject("pathValue1", vars.get("pathValue1"));
+		mNv.addObject("pathValue2", vars.get("pathValue2"));
 		return mNv;
 	}
 }
