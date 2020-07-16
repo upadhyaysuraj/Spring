c09ea223ed2c081e454a7f1b78051663d1bd18fc
Suraj <su.suraj.upadhyay@gmail.com>
2020-07-16 16:22:14 -0400

    REST-CURD operations Successful!!! input -> FORM Output -> JSON data

--- Spring_08_JdbcTemplate/src/main/java/com/java/spring/controller/ProductController.java
+++ Spring_08_JdbcTemplate/src/main/java/com/java/spring/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.java.spring.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.java.spring.model.Product;
 import com.java.spring.service.ProductService;
@@ -20,4 +24,27 @@ public class ProductController
 		return p;
 	}
 	
+	@PostMapping(value="/deleteProduct")
+	public boolean deleteProduct(@RequestParam int id)
+	{
+		return productService.deleteProduct(id);
+	}
+	
+	@PostMapping("/updateProduct")
+	public boolean updateProduct(@ModelAttribute Product p)
+	{
+		return productService.updateProduct(p, p.getPID());
+	}
+	
+	@GetMapping("/getAllProducts")
+	public List<Product> getAllProducts()
+	{
+		return productService.getAllProducts();
+	}
+	
+	@PostMapping("/addProduct")
+	public boolean addProduct(@ModelAttribute Product p)
+	{
+		return productService.addProduct(p);
+	}
 }
