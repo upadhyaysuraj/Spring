40306084a097056442d8113ff1082a63bcd01450
Suraj <su.suraj.upadhyay@gmail.com>
2020-07-22 10:50:06 -0400

    -

---
+++ SpringBoot_01_FirtsApp/src/main/java/com/java/spring/SpringBoot01FirtsAppApplication.java
@@ -0,0 +1,13 @@
+package com.java.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01FirtsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot01FirtsAppApplication.class, args);
+	}
+
+}
